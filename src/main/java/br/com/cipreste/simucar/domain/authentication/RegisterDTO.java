010916b061efd155a540c5ae85839ba4b32e07db
CarlosCipreste <carlos3215910@hotmail.com>
2024-10-31 00:28:28 -0300

    Mudança de Record para class

--- src/main/java/br/com/cipreste/simucar/domain/authentication/RegisterDTO.java
+++ src/main/java/br/com/cipreste/simucar/domain/authentication/RegisterDTO.java
@@ -1,11 +1,44 @@
 package br.com.cipreste.simucar.domain.authentication;
 
-public record RegisterDTO(
-        String login,
-        String password,
-        String nome,
-        String sobrenome,
-        String cpf,
-        String email,
-        String numeroCelular) {
+import org.hibernate.validator.constraints.br.CPF;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class RegisterDTO {
+
+    @NotBlank
+    private String login;
+
+    @NotBlank
+    private String password;
+
+    @NotBlank
+    private String nome;
+
+    @NotBlank
+    private String sobrenome;
+
+    @CPF
+    private String cpf;
+
+    @Email
+    private String email;
+
+    @JsonProperty("numero_celular")
+    @Pattern(regexp = "^\\(\\d{2}\\) \\d{5}-\\d{4}$", message = "Número de celular inválido")
+    private String numeroCelular;
 }
--- src/main/java/br/com/cipreste/simucar/domain/security/SecurityConfig.java
+++ src/main/java/br/com/cipreste/simucar/domain/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package br.com.cipreste.simucar.domain.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,13 +10,17 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll() 
+                        .requestMatchers("/auth/**").permitAll()
+                        .anyRequest().permitAll()
+                )
                 .build();
     }
-
 }
